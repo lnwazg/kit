b5bdc550f3c564952e24990b8709637235ef744d
lnwazg <lnwazg@126.com>
2018-10-30 01:16:08 +0800

    转换器增强

--- src/main/java/com/lnwazg/kit/converter/abst/AbstractDbConverter.java
+++ src/main/java/com/lnwazg/kit/converter/abst/AbstractDbConverter.java
@@ -1,7 +1,5 @@
 package com.lnwazg.kit.converter.abst;
 
-import java.sql.SQLException;
-
 import com.lnwazg.kit.converter.ValueConverter;
 import com.lnwazg.kit.converter.iface.DbConvertAware;
 
@@ -18,84 +16,77 @@ public abstract class AbstractDbConverter implements DbConvertAware
      * @author nan.li
      * @param key
      * @return
-     * @throws SQLException 
+     * @ 
      */
-    public abstract Object convertValue(String key)
-        throws SQLException;
-        
+    public abstract Object convertValue(String key);
+    
+    public String get(String key)
+    {
+        return convert2ValueConverter(key).get();
+    }
+    
+    @Override
+    public String getAsString(String key)
+    {
+        return getAs(key, String.class);
+    }
+    
+    @Override
+    public boolean getAsBoolean(String key)
+    {
+        return getAs(key, Boolean.class);
+    }
+    
+    @Override
+    public double getAsDouble(String key)
+    {
+        return getAs(key, Double.class);
+    }
+    
+    @Override
+    public float getAsFloat(String key)
+    {
+        return getAs(key, Float.class);
+    }
+    
+    @Override
+    public long getAsLong(String key)
+    {
+        return getAs(key, Long.class);
+    }
+    
+    @Override
+    public int getAsInt(String key)
+    {
+        return getAs(key, Integer.class);
+    }
+    
+    @Override
+    public byte getAsByte(String key)
+    {
+        return getAs(key, Byte.class);
+    }
+    
+    @Override
+    public short getAsShort(String key)
+    {
+        return getAs(key, Short.class);
+    }
+    
+    @Override
+    public <T> T getAs(String key, Class<T> clazz)
+    {
+        return convert2ValueConverter(key).getAs(clazz);
+    }
+    
     /**
     * 转换成一个值转换器
     * @author nan.li
     * @param key
     * @return
-     * @throws SQLException 
     */
-    public ValueConverter convert2ValueConverter(String key)
-        throws SQLException
+    private ValueConverter convert2ValueConverter(String key)
     {
         return new ValueConverter(convertValue(key));
-    }
-    
-    @Override
-    public String getAsString(String key)
-        throws SQLException
-    {
-        return convert2ValueConverter(key).getAsString();
-    }
-    
-    @Override
-    public boolean getAsBoolean(String key)
-        throws SQLException
-    {
-        return convert2ValueConverter(key).getAsBoolean();
-    }
-    
-    @Override
-    public double getAsDouble(String key)
-        throws SQLException
-    {
-        return convert2ValueConverter(key).getAsDouble();
-    }
-    
-    @Override
-    public float getAsFloat(String key)
-        throws SQLException
-    {
-        return convert2ValueConverter(key).getAsFloat();
-    }
-    
-    @Override
-    public long getAsLong(String key)
-        throws SQLException
-    {
-        return convert2ValueConverter(key).getAsLong();
-    }
-    
-    @Override
-    public int getAsInt(String key)
-        throws SQLException
-    {
-        return convert2ValueConverter(key).getAsInt();
-    }
-    
-    @Override
-    public byte getAsByte(String key)
-        throws SQLException
-    {
-        return convert2ValueConverter(key).getAsByte();
-    }
-    
-    @Override
-    public short getAsShort(String key)
-        throws SQLException
-    {
-        return convert2ValueConverter(key).getAsShort();
-    }
-    
-    @Override
-    public <T> T getAs(String key, Class<T> clazz)
-        throws SQLException
-    {
-        return convert2ValueConverter(key).getAs(clazz);
     }
 }
