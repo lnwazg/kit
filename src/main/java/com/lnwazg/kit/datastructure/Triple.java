a24943e2debcc38baad1c46982880a8452eeab14
lnwazg <lnwazg@126.com>
2019-11-28 01:23:40 +0800

    Update Triple.java

--- src/main/java/com/lnwazg/kit/datastructure/Triple.java
+++ src/main/java/com/lnwazg/kit/datastructure/Triple.java
@@ -5,56 +5,67 @@ package com.lnwazg.kit.datastructure;
  * @author nan.li
  * @version 2018年5月16日
  */
-public class Triple<L,M,R> {
+public class Triple<L, M, R>
+{
     private L l;
+    
     private M m;
+    
     private R r;
-
-    public Triple() {
+    
+    public Triple()
+    {
     }
-
-    public Triple(L left,M middle, R right) {
+    
+    public Triple(L left, M middle, R right)
+    {
         this.l = left;
         this.m = middle;
         this.r = right;
     }
-
-    public L getLeft() {
+    
+    public L getL()
+    {
         return l;
     }
-
-    public void setLeft(L l) {
+    
+    public void setL(L l)
+    {
         this.l = l;
     }
-
-    public R getRight() {
-        return r;
-    }
-
-    public void setRight(R r) {
-        this.r = r;
-    }
-
+    
     public M getM()
     {
         return m;
     }
-
+    
     public void setM(M m)
     {
         this.m = m;
     }
-
-	/**
-	 * 根据等号左边的泛型，自动构造合适的Triple
-	 */
-	public static <L, M, R> Triple<L, M, R> of(L left, M middle, R right) {
-		return new Triple<L, M, R>(left, middle, right);
-	}
-
-	@Override
-	public String toString() {
-		return "Triple [l=" + l + ", m=" + m + ", r=" + r + "]";
-	}
+    
+    public R getR()
+    {
+        return r;
+    }
+    
+    public void setR(R r)
+    {
+        this.r = r;
+    }
+    
+    /**
+     * 根据等号左边的泛型，自动构造合适的Triple
+     */
+    public static <L, M, R> Triple<L, M, R> of(L left, M middle, R right)
+    {
+        return new Triple<L, M, R>(left, middle, right);
+    }
+    
+    @Override
+    public String toString()
+    {
+        return "Triple [l=" + l + ", m=" + m + ", r=" + r + "]";
+    }
     
 }
